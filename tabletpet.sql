ed63b46812fa74da4a7cc8a0740bf4142e0a63e4
Denver <denver.gomes@sga.pucminas.br>
2022-04-20 18:24:09 -0300

    Banco de dados

---
+++ tabletpet.sql
@@ -0,0 +1,13 @@
+create database dbpet;
+rename database pet to dbpet;
+use dbpet;
+Create table Pet (idanimal,nome,especie,sexo,idade,porte,cor,localizacao,descricao,statusanimal)
+);
+
+drop database comercioti;
+
+select * from pet;
+
+insert into pet values ('080','Garfield','gato','macho','2','médio','branco','Minas Gerais','manso','adotado');
+
+use pet;

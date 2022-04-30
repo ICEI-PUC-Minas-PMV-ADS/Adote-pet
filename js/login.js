f3ba2b080f736f8fffa4b13dd3eff40c1e710835
Fabio Furlan <furlan.fabio@hotmail.com>
2022-04-30 20:26:11 -0300

    login_modal

--- js/login.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('button').addEventListener('click', function() {
-    document.querySelector('.bg-modal').style.display = 'flex';
-});
-
-document.querySelector('.close').addEventListener('click', function() {
-    document.querySelector('.bg-modal').style.display = 'none';
-});

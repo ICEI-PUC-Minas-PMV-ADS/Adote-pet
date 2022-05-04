843f35ba386a13e969f36e99e54593db9aec4fc2
BGammaro <90659728+BGammaro@users.noreply.github.com>
2022-05-04 11:07:59 -0300

    index

--- login.cs
+++ login.cs
@@ -1,17 +1,34 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data.Linq.mapping;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
 
 
-namespace consoleApplicatin1
+static void main(string[] args)
 {
-    class login
+    string email, temail, senha, tsenha;
+    Console.Write("E-mail");
+    email = readline();
+    Console.Write("senha");
+    senha = readline();
+    try
     {
-        static void main(string[] args)
+        OpenConnection();
+        
+    }
+    if (email = temail)
+    {
+        if(senha = tsenha)
         {
 
         }
+        else
+        Console.Write("Senha Invalida");
     }
+    else 
+    Console.Write("Email invalido");
 }

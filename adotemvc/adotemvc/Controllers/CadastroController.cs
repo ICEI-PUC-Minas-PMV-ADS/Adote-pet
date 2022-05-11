5e32902736565265deaeba27737ecb18847f4d4e
Denver <denver.gomes@sga.pucminas.br>
2022-05-11 16:38:52 -0300

    MVC cadastro pet

---
+++ adotemvc/adotemvc/Controllers/CadastroController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace adotemvc.Controllers
+{
+    public class CadastroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult ApagarConfirmacao()
+        {
+            return View();
+        }
+    }
+}

8b15c17074462a526b1791ee74d74c1169079a41
John <denver.gomes@sga.pucminas.br>
2022-05-15 21:02:24 -0300

    Azure

--- src/Projeto-Adote-Pet/Projeto-Adote-Pet/obj/Release/net5.0/Projeto-Adote-Pet.AssemblyInfo.cs
+++ src/Projeto-Adote-Pet/Projeto-Adote-Pet/obj/Release/net5.0/Projeto-Adote-Pet.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("eb51a74b-164f-4301-a2e9-8531d3d973de")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto-Adote-Pet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]

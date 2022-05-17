6428fe5a9800ac41241f2ed32932de90fbbf9b11
John <denver.gomes@sga.pucminas.br>
2022-05-16 21:38:16 -0300

    Pasta GitAdotemvc para teste cadastropet

---
+++ src/GitAdotemvc/adotemvc/Models/CadastroModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace adotemvc.Models
+{
+    public class CadastroModel
+    {
+        [Key]
+        public int Idanimal { get; set; }
+        public string Nome { get; set; }
+        public string Especie { get; set; }
+        public string Sexo { get; set; }
+        public string Raça { get; set; }
+        public string Idade { get; set; }
+        public string Porte { get; set; }
+        public string Cor { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public string Descrição { get; set; }
+        public string Pstatus { get; set; }
+
+
+
+
+
+
+    }
+}
---
+++ src/GitAdotemvc/adotemvc/Models/UsuarioModel.cs
@@ -0,0 +1,21 @@
+﻿using adotemvc.Enum;
+using System;
+
+namespace adotemvc.Models
+{
+    public class UsuarioModel
+    {
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+        //Enumerador está na pasta Enum
+        public PerfilEnum Perfil { get; set; }
+        
+        //? campo pode ser nulo
+        public DateTime? DataAtualizacao { get; set; }
+
+    }
+}

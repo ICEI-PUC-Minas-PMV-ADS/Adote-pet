9f89790224f1fdd583672e9d51d38e1a13e2d2a1
John <denver.gomes@sga.pucminas.br>
2022-05-17 00:35:14 -0300

    Carrossel no Index

---
+++ src/Projeto-Adote-Pet/Projeto-Adote-Pet/Controllers/CarrosselController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Projeto_Adote_Pet.Controllers
+{
+    public class CarrosselController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

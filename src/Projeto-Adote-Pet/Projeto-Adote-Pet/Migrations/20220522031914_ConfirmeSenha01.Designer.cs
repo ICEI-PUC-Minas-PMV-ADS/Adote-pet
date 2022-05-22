f3e35c8fd762322260dcc53a0d59588c5110e53d
John <denver.gomes@sga.pucminas.br>
2022-05-22 00:48:54 -0300

    Confirmação de senha

--- src/Projeto-Adote-Pet/Projeto-Adote-Pet/Migrations/20220522031914_ConfirmeSenha01.Designer.cs
+++ src/Projeto-Adote-Pet/Projeto-Adote-Pet/Migrations/20220522031914_ConfirmeSenha01.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Projeto_Adote_Pet.Models;
 
 namespace Projeto_Adote_Pet.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220522031914_ConfirmeSenha01")]
+    partial class ConfirmeSenha01
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- src/Projeto-Adote-Pet/Projeto-Adote-Pet/wwwroot/js/site.js
+++ src/Projeto-Adote-Pet/Projeto-Adote-Pet/wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//Confirmação de senha
+function checkSenha() {
+
+    var senha1 = document.getElementById("Senha").value;
+    var senha2 = document.getElementById("ConfirmeSenha").value;
+
+    if (senha1 == senha2) {
+        document.getElementById("msgsenha").style.color = "green";
+        document.getElementById("msgsenha").innerHTML = "Senhas confirmadas";        
+    }
+    else {
+        document.getElementById("msgsenha").style.color = "red";
+        document.getElementById("msgsenha").innerHTML = "Senhas NÃO confirmadas";
+        
+    }
+}

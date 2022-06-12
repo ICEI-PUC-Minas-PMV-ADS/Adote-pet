f24eea89271ad59e33c9eb56624842a90960acde
John <denver.gomes@sga.pucminas.br>
2022-06-12 19:13:09 -0300

    Retirado código com erro

--- src/Projeto-Adote-Pet/Projeto-Adote-Pet/Controllers/PetController.cs
+++ src/Projeto-Adote-Pet/Projeto-Adote-Pet/Controllers/PetController.cs
@@ -188,27 +188,7 @@ namespace Projeto_Adote_Pet.Controllers
         }
         //Inicio Imagem Pet
 
-        public byte[] Imagem { get; set; }
-
-        public class Conversor
-        {
-            public static byte[] ImagemParaByte(Image imagem)
-            {
-                using (var stream = new MemoryStream())
-                {
-                    imagem.Save(stream, ImageFormat.Png);
-                    return stream.ToArray();
-                }
-            }
-
-            public static Image ByteParaImagem(byte[] bytes)
-            {
-                using (var stream = new MemoryStream(bytes))
-                {
-                    return Image.FromStream(stream);
-                }
-            }
-        }
+        
         // fim imagem
     }
 }

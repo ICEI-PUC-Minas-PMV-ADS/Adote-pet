23aac8026ab99aa0ee7604fbcaca3fd46a8153df
Denver Gonçalves Gomes <90660885+denverbh@users.noreply.github.com>
2022-06-23 00:05:25 -0300

    Exclusão de pasta sem uso
    Tratava-se de uma pasta temporária

--- src/GitAdotemvc/adotemvc/Enum/PerfilEnum.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace adotemvc.Enum
-{
-    //possibilita atribui valor para perfis, pode ser aplicado ao CPF e CNPJ
-    public enum PerfilEnum
-    {
-            Admin = 1,
-            padrao = 2,
-    }
-}
